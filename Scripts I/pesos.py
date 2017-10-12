5794f55842847a8265af5e0c56598a624af0a99c
Alicia Suarez <marialiciasuarezcastro@gamil.com>
2017-10-12 14:37:31 -0400

    listas 10 pruebas

--- Scripts I/pesos.py
+++ Scripts I/pesos.py
@@ -1,551 +1,263 @@
 # generador pesos
 
 import random
+
+def mochila(lista,cap):
+	capacidad=cap
+	Beneficio_total=0
+	Continuar=True
+	Mochila=[]
+	intentos_fallidos=25
+
+	while capacidad>0 or Continuar:
+
+			if len(lista)==0:
+				break
+
+			indice=random.randrange(len(lista))
+			elemento=lista[indice]
+			lista.remove(elemento)
+
+			if (capacidad-elemento[0]>=0):
+				intentos_fallidos=25
+				capacidad=capacidad-elemento[0]
+				Beneficio_total=Beneficio_total+elemento[1]
+				Mochila=Mochila+[elemento]
+			else:
+				intentos_fallidos=intentos_fallidos-1
+
+
+			if intentos_fallidos==0:
+				if len(lista)==0:
+					break
+				elemento=lista[0]
+				if capacidad-elemento[0]>=0:
+					intentos_fallidos=25
+				else:
+					Continuar=False
+
+	return Beneficio_total
+	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
+	#print Mochila
+	
 pb=[]
 
-for i in range(0,9):
-	aux=(random.randrange(19)+1,random.randrange(19)+1)
-	pb=pb+[aux]
+for i in range(0,39):
+	pb=pb+[(random.randrange(99)+1,random.randrange(99)+1)]
 
 pb=sorted(pb)
+
+capacidad=random.randrange(500)+50
+
 print pb
-capacidad=random.randrange(50)+1
-capacidad2=capacidad
-
-
-#generador de pesos[0] y beneficios[1]
-#
-
-Continuar=True
-Mochila=[]
+print "Capacidad Inicial: "+str(capacidad)
 
 Beneficio_total=0
-intentos_fallidos=25
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
 
-print "Capacidad Inicial: "+str(capacidad)
-	
+
+
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
 
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
+
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
 
-	
 for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
-
-
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
+	
+	
+	Beneficio_total=mochila([(1, 51), (2, 80), (4, 38), (10, 85), (18, 89), (21, 94), (25, 63), (27, 42), (28, 46), (29, 83), (35, 12), (36, 53), (37, 88), (40, 60), (46, 80), (47, 23), (50, 22), (56, 16), (56, 59), (60, 73), (61, 11), (63, 27), (66, 21), (71, 94), (76, 90), (78, 88), (79, 33), (79, 84), (80, 54), (81, 54), (82, 24), (84, 73), (85, 65), (92, 97), (93, 9), (93, 24), (95, 36), (96, 50), (97, 78)]
+		,capacidad)
 
 
 	if Beneficio_total>Mayor_Beneficio:
 		Mayor_Beneficio=Beneficio_total
 		Beneficios=Beneficios+[Mayor_Beneficio]
 		indices=indices+[i]
-print Beneficios
 
+print Beneficios
 print indices
+
+Beneficio_total=0
 Beneficios=[]
 Mayor_Beneficio=0
 indices=[]
-#print pb2
-#for i in range (0,10):
-
-	
-for i in range(1,10000):
-	Continuar=True
-	Mochila=[]
-
-	Beneficio_total=0
-	intentos_fallidos=25
-
-	capacidad=capacidad2
 
 
-
-	while capacidad>0 or Continuar:
-			
-			indice=random.randrange(len(pb))
-			elemento=pb[indice]
-
-			if (capacidad-elemento[0]>=0):
-				intentos_fallidos=25
-				capacidad=capacidad-elemento[0]
-				Beneficio_total=Beneficio_total+elemento[1]
-				Mochila=Mochila+[elemento]
-			else:
-				intentos_fallidos-=1
-
-			if intentos_fallidos==0:
-				elemento=pb[0]
-				if capacidad-elemento[0]>=0:
-					intentos_fallidos=25
-				else:
-					Continuar=False
-	#print "Beneficio: "+str(Beneficio_total)+ " Capacidad: "+ str(capacidad)
-	#print Mochila
-
-
-	if Beneficio_total>Mayor_Beneficio:
-		Mayor_Beneficio=Beneficio_total
-		Beneficios=Beneficios+[Mayor_Beneficio]
-		indices=indices+[i]
-print Beneficios
-print indices
---
+++ Scripts I/prueba.py
@@ -0,0 +1,8 @@
+def imprimir(lista):
+	for i in lista:
+		print i
+
+
+a=[3,4,5,3,6,3]
+
+imprimir(a)
