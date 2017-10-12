a36dc62f0ddb4d82b50cf3c893761ed635b197e1
Sergio Teran <sergioteran.zambrano@gmail.com>
2017-10-12 15:55:57 -0400

    Pruebas en Haskell

--- Scripts I/Optimo.hs
+++ Scripts I/Optimo.hs
@@ -11,6 +11,24 @@
 --	13/10/2017
 -- --------------------------------------------------
 
-mochila :: [(Int,Int)] -> Int -> Int
-mochila [] _ = 0
-mochila ((pj,bj):xs) c = if c-pj < 0 then 0 else (mochila xs c) `max`  ((mochila xs (c-pj)) + bj)
+module Optimo where
+import Data.List
+import Data.Function(on)
+
+-- SOLUCION EXPONENCIAL
+mochila' :: [(Int,Int)] -> Int -> Int
+mochila' [] _ = 0
+mochila' ((pj,bj):xs) c = if c-pj < 0 then 0 else (mochila' xs c) `max`  ((mochila' xs (c-pj)) + bj)
+-------------------
+
+-- version CHINO+MANDARIN AVANZADO
+mochila :: [(Int, Int)] -> [(Int, Int)] -> Int -> [(Int, Int)]
+mochila xs [] _   = xs
+mochila xs ys max =
+    foldr (maxDe) [] (xs: [mochila (y:xs) (delete y ys) max | y <- ys, peso(y:xs) <= max ] ) 
+                           where
+                             peso = sum . map snd
+
+maxDe :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+maxDe a b = maximumBy (compare `on` valueOf) [a,b] where
+            valueOf = sum . map fst
