9dc45e6b01a356ce4249c27b98e6f83f1b96cee8
Alicia Suarez <marialiciasuarezcastro@gamil.com>
2017-10-13 09:21:52 -0400

    holis

--- Scripts I/prueba.py
+++ Scripts I/prueba.py
@@ -1,8 +1,23 @@
-def imprimir(lista):
-	for i in lista:
-		print i
+import random
 
 
-a=[3,4,5,3,6,3]
+pb2=[(1, 9), (1, 19), (2, 13), (3, 8), (4, 3), (4, 14), (6, 14), (10, 13), (16, 12)]
+pb=[]	
+for i in pb2:
+	costo=i[0]
+	beneficio=i[1]
+	pb=pb+[(beneficio/costo,costo,beneficio)]
+pb=sorted(pb)
 
-imprimir(a)
+#print pb
+
+capacidad=11
+beneficio=0
+for i in pb:
+	if capacidad-i[1]>0:
+		capacidad=capacidad-i[1]
+		beneficio=beneficio+i[2]
+
+print pb
+print "Capacidad Inicial: "+str(capacidad)
+print beneficio
